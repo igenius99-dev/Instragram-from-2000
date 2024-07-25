3f6fa043b6e8f40c57769c1f4657a62c90904d11
igenius99-dev <tusharsachan06@gmail.com>
2024-07-25 06:28:59 +0530

    Initial commit

---
+++ src/user/pages/Users.js
@@ -0,0 +1,18 @@
+import React from "react";
+import UserList from "../components/UserList";
+
+const Users = () => {
+  const USERS = [
+    {
+      id: "1",
+      image:
+        "https://cdn.motor1.com/images/mgl/zxxMzK/s1/2023-porsche-911-gt3-rs.webp",
+      name: "yessirr",
+      places: 3,
+    },
+  ];
+
+  return <UserList items={USERS} />;
+};
+
+export default Users;
